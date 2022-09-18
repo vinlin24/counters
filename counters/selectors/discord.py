d91a135c7cb7f3c83346192c1d2cbdd1bb6bf7f4
Vincent Lin <soulace2425@outlook.com>
2022-09-17 20:24:38 -0700

    Use CSS selectors instead of xpaths for discord

---
+++ counters/selectors/discord.py
@@ -0,0 +1,19 @@
+"""discord.py
+
+Defines CSS selectors for Selenium scraping of the Discord web app.
+"""
+
+EMAIL_INPUT = "#uid_5"
+"""The text input box for email on the login page."""
+
+PASSWORD_INPUT = "#uid_8"
+"""The text input box for password on the login page."""
+
+AVATAR_ICON = ".avatar-1EWyVD.wrapper-1VLyxH"
+"""The circular, clickable avatar icon in the bottom left."""
+
+EDIT_STATUS_ITEM = "#account-edit-custom-status"
+"""The "Edit Custom Status" menu item in the avatar menu popup."""
+
+STATUS_INPUT = ".inputDefault-3FGxgL.input-2g-os5.input-3AAlUz"
+"""The text input box that appear upon clicking Edit Custom Status."""
--- counters/update_discord.py
+++ counters/update_discord.py
@@ -4,25 +4,13 @@ Interface for updating Discord custom status.
 """
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from .config import DISCORD_EMAIL, DISCORD_PASSWORD
-
-# ==================== FULL XPATHS ==================== #
-# These risk changing every damn update :/
-
-XPATH_LOGIN_BUTTON = ("/html/body/div[1]/div/div/div[1]/div[1]/header[1]/nav/"
-                      "div/a")
-XPATH_EMAIL_INPUT = ("/html/body/div[1]/div[2]/div/div[1]/div/div/div/div/"
-                     "form/div/div/div[1]/div[2]/div[1]/div/div[2]/input")
-XPATH_PASSWORD_INPUT = ("/html/body/div[1]/div[2]/div/div[1]/div/div/div/div/"
-                        "form/div/div/div[1]/div[2]/div[2]/div/input")
-XPATH_AVATAR_ICON = ("/html/body/div[2]/div[2]/div/div[1]/div/div[2]/div/"
-                     "div[1]/div/div/div[1]/section/div[2]/div[1]/div[1]")
-XPATH_CUSTOM_STATUS = ("/html/body/div[2]/div[2]/div/div[3]/div/div/div/div/"
-                       "div[3]/div[2]/div[1]")
-XPATH_STATUS_INPUT = ("/html/body/div[2]/div[2]/div/div[3]/div[2]/div/div/"
-                      "div[2]/div[1]/div[2]/div/div[2]/input")
-
+# Experimenting CSS selectors as an alternative to full XPaths
+# Not sure how often these will change in comparison
+from .selectors.discord import (AVATAR_ICON, EDIT_STATUS_ITEM, EMAIL_INPUT,
+                                PASSWORD_INPUT, STATUS_INPUT)
 
 # ==================== SCRAPING SUBROUTINES ==================== #
 
@@ -34,14 +22,8 @@ def _login(driver: webdriver.Edge) -> None:
         driver (webdriver.Edge): Edge web driver instance.
     """
     # Find elements
-    email_input = driver.find_element(
-        "xpath",
-        XPATH_EMAIL_INPUT
-    )
-    password_input = driver.find_element(
-        "xpath",
-        XPATH_PASSWORD_INPUT
-    )
+    email_input = driver.find_element(By.CSS_SELECTOR, EMAIL_INPUT)
+    password_input = driver.find_element(By.CSS_SELECTOR, PASSWORD_INPUT)
 
     # Enter credentials
     email_input.clear()
@@ -58,24 +40,15 @@ def _update_status(driver: webdriver.Edge, status: str) -> None:
         status (str): Status to input as the new custom status.
     """
     # Bring up menu in the bottom left corner
-    avatar_icon = driver.find_element(
-        "xpath",
-        XPATH_AVATAR_ICON
-    )
+    avatar_icon = driver.find_element(By.CSS_SELECTOR, AVATAR_ICON)
     avatar_icon.click()
 
     # Click the "Edit custom status" option
-    custom_status = driver.find_element(
-        "xpath",
-        XPATH_CUSTOM_STATUS
-    )
+    custom_status = driver.find_element(By.CSS_SELECTOR, EDIT_STATUS_ITEM)
     custom_status.click()
 
     # Get the input text bot
-    status_input = driver.find_element(
-        "xpath",
-        XPATH_STATUS_INPUT
-    )
+    status_input = driver.find_element(By.CSS_SELECTOR, STATUS_INPUT)
 
     # Enter the status into the text box
     status_input.clear()
