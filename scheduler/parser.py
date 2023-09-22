efa9b3d06ed9c628881be5d7f5e88ac853155efa
Vincent Lin <vinlin24@outlook.com>
2023-09-21 17:07:43 -0700

    Remove unused files (discontinuing scheduler pkg)

--- scheduler/parser.py
+++
@@ -1,8 +0,0 @@
-"""Defines the command line parser for this package."""
-
-from argparse import ArgumentParser
-
-parser = ArgumentParser(
-    description="Add the counters package to the Windows Task Scheduler"
-)
-parser.add_argument("path")
