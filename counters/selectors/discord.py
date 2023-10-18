691c2b755e1d00a942d694e148224129ee5f12f3
Vincent Lin <vinlin24@outlook.com>
2023-10-18 15:42:07 -0700

    Fix selectors for PW, avatar icon, status input

--- counters/selectors/discord.py
+++ counters/selectors/discord.py
@@ -6,10 +6,10 @@ Defines CSS selectors for Selenium scraping of the Discord web app.
 EMAIL_INPUT = "#uid_5"
 """The text input box for email on the login page."""
 
-PASSWORD_INPUT = "#uid_8"
+PASSWORD_INPUT = "#uid_7"
 """The text input box for password on the login page."""
 
-AVATAR_ICON = ".avatar-1EWyVD.wrapper-1VLyxH"
+AVATAR_ICON = ".avatar-19siqs"
 """The circular, clickable avatar icon in the bottom left."""
 
 SET_STATUS_ITEM = "#account-set-custom-status"
@@ -18,5 +18,5 @@ SET_STATUS_ITEM = "#account-set-custom-status"
 EDIT_STATUS_ITEM = "#account-edit-custom-status"
 """The "Edit Custom Status" menu item in the avatar menu popup."""
 
-STATUS_INPUT = ".inputDefault-3FGxgL.input-2g-os5.input-3AAlUz"
+STATUS_INPUT = ".inputDefault-Ciwd-S"
 """The text input box that appear upon clicking Edit Custom Status."""
