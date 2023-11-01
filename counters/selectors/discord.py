e5af20353687b32f28c0df30c8c997a5810a9a33
Vincent Lin <vinlin24@outlook.com>
2023-11-01 01:57:16 -0700

    Update Discord selectors

--- counters/selectors/discord.py
+++ counters/selectors/discord.py
@@ -9,7 +9,7 @@ EMAIL_INPUT = "#uid_5"
 PASSWORD_INPUT = "#uid_7"
 """The text input box for password on the login page."""
 
-AVATAR_ICON = ".avatar-19siqs"
+AVATAR_ICON = "#app-mount > div.appAsidePanelWrapper__714a6 > div.notAppAsidePanel__9d124 > div.app_b1f720 > div > div.layers__1c917.layers_a23c37 > div > div > div > div > div > section > div.container_ca50b9 > div.avatarWrapper_ba5175.withTagAsButton_cc125f > div.wrapper_edb6e0.avatar_f8541f"
 """The circular, clickable avatar icon in the bottom left."""
 
 SET_STATUS_ITEM = "#account-set-custom-status"
@@ -18,5 +18,5 @@ SET_STATUS_ITEM = "#account-set-custom-status"
 EDIT_STATUS_ITEM = "#account-edit-custom-status"
 """The "Edit Custom Status" menu item in the avatar menu popup."""
 
-STATUS_INPUT = ".inputDefault-Ciwd-S"
+STATUS_INPUT = ".inputDefault__80165"
 """The text input box that appear upon clicking Edit Custom Status."""
