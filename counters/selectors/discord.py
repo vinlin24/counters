6892a2d4292480903073c8798640d2319c87c164
Vincent Lin <vinlin24@outlook.com>
2024-02-07 23:41:44 -0800

    Update selectors for Discord login page

--- counters/selectors/discord.py
+++ counters/selectors/discord.py
@@ -3,10 +3,10 @@
 Defines CSS selectors for Selenium scraping of the Discord web app.
 """
 
-EMAIL_INPUT = "#uid_5"
+EMAIL_INPUT = "#uid_7"
 """The text input box for email on the login page."""
 
-PASSWORD_INPUT = "#uid_7"
+PASSWORD_INPUT = "#uid_9"
 """The text input box for password on the login page."""
 
 AVATAR_ICON = "#app-mount > div.appAsidePanelWrapper__714a6 > div.notAppAsidePanel__9d124 > div.app_b1f720 > div > div.layers__1c917.layers_a23c37 > div > div > div > div > div > section > div.container_ca50b9 > div.avatarWrapper_ba5175.withTagAsButton_cc125f > div.wrapper_edb6e0.avatar_f8541f"
