5de5d3691d107a2cbdfe90b41f6eb881a5fab08c
Vincent Lin <vinlin24@outlook.com>
2024-03-11 08:07:29 -0700

    Write InstagramUpdater

--- counters/updaters/instagram.py
+++ counters/updaters/instagram.py
@@ -3,16 +3,112 @@
 Interface for updating Instagram bio.
 """
 
+from datetime import date
+from typing import TypedDict
+
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+from ..bios import day_number
 from ..config import INSTAGRAM_PASSWORD, INSTAGRAM_USERNAME
 from ..selectors.instagram import (BIO_BOX, LOGIN_BUTTON, NOT_NOW_BUTTON,
                                    PASSWORD_INPUT, SUBMIT_BUTTON,
                                    USERNAME_INPUT)
+from ..updaters.base import Updater
+
+
+class InstagramDetails(TypedDict):
+    bio: str | None
+
+
+class InstagramUpdater(Updater[InstagramDetails]):
+    def prepare_details(self, today: date) -> InstagramDetails:
+        task: dict = self.data["instagram"]
+
+        # Fill placeholder in bio template if provided
+        start: date | None = task["start"]
+        bio: str | None = task["bio"]
+        if start is not None and bio is not None:
+            bio = bio.format(day_number(start, today))
+
+        return {"bio": bio}
+
+    def update_bio(self, details: InstagramDetails) -> None:
+        bio = details["bio"]
+        if bio is None:
+            return
+        self.driver.get("https://www.instagram.com/accounts/edit")
+        self._login()
+        self._navigate_to_profile()
+        self._update_profile(bio)
+
+    def _login(self) -> None:
+        """Handle the authentication landing page."""
+        # Find elements
+        username_elem = self.driver.find_element(*USERNAME_INPUT)
+        password_elem = self.driver.find_element(*PASSWORD_INPUT)
+        login_button = self.driver.find_element(*LOGIN_BUTTON)
+
+        # Input credentials and login
+        username_elem.clear()
+        username_elem.send_keys(INSTAGRAM_USERNAME)
+        password_elem.clear()
+        password_elem.send_keys(INSTAGRAM_PASSWORD)
+        login_button.click()
+
+    def _navigate_to_profile(self) -> None:
+        """
+        Handle navigating to the profile edit page after
+        authenticated.
+        """
+        # Dismiss the "Save login info" if it appears
+        condition = EC.presence_of_element_located(
+            (NOT_NOW_BUTTON.by, NOT_NOW_BUTTON.value)
+        )
+        try:
+            not_now_button: WebElement = \
+                WebDriverWait(self.driver, 5).until(condition)
+            not_now_button.click()
+        except TimeoutException:
+            # Just try to redirect again bro sigh
+            self.driver.get("https://www.instagram.com/accounts/edit")
+
+    def _update_profile(self, bio: str) -> None:
+        """Handle updating the bio after reaching the edit profile page.
+
+        Args:
+            bio (str): Bio to input as the new bio.
+
+        Raises:
+            TimeoutException: Couldn't locate a certain element within
+            `WAIT_TIMEOUT` seconds.
+        """
+        # Find elements
+        bio_box = self.driver.find_element(*BIO_BOX)
+
+        # Submit new bio string
+        bio_box.clear()
+        bio_box.send_keys(bio)
+
+        # NOTE: If you don't edit anything, the button will be disabled
+        submit_button = self.driver.find_element(*SUBMIT_BUTTON)
+        submit_button.click()
+
+        # Make sure the update registered
+        # try:
+        #     WebDriverWait(self.driver, WAIT_TIMEOUT).until(
+        #         EC.presence_of_element_located((By.XPATH, XPATH_PROFILE_SAVED))
+        #     )
+        # except TimeoutException:
+        #     print("Couldn't locate 'Profile saved' flash notification.")
+        #     log.error("Couldn't locate 'Profile saved' flash notification.")
+        #     raise
+
+
+# TODO: Replace below when finished refactoring other modules.
 
 
 def _login(driver: webdriver.Edge) -> None:
