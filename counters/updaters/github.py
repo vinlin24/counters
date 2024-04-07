2bc4f95d82e046a350f280dff82ae8ae69f9159c
Vincent Lin <vinlin24@outlook.com>
2024-04-06 21:58:47 -0700

    Remove old standalone updater functions

--- counters/updaters/github.py
+++ counters/updaters/github.py
@@ -48,23 +48,3 @@ class GitHubUpdater(Updater[GitHubDetails]):
             body=details["bio"],
             color="white",
         )
-
-
-# TODO: Replace below when finished refactoring other modules.
-# ==================== INTERFACE FUNCTION ==================== #
-
-
-def update_profile_bio(bio: str | None) -> None:
-    """Update GitHub profile bio with new text.
-
-    Args:
-        status (str | None, optional): New bio text. Defaults to None,
-        meaning leave the status unchanged, in which case, this function
-        does nothing.
-    """
-    if bio is None:
-        return
-    auth = Auth.Token(GITHUB_PAT)
-    github = Github(auth=auth)
-    user = github.get_user()
-    user.edit(bio=bio)
--- counters/updaters/spotify.py
+++ counters/updaters/spotify.py
@@ -94,35 +94,3 @@ class SpotifyPlaylistUpdater(Updater[SpotifyPlaylistDetails]):
             body=table,
             color="green",
         )
-
-
-# TODO: Replace below when finished refactoring other modules.
-
-
-# Authenticate
-token = tekore.refresh_user_token(client_id=SPOTIFY_CLIENT_ID,
-                                  client_secret=SPOTIFY_CLIENT_SECRET,
-                                  refresh_token=SPOTIFY_USER_REFRESH)
-
-# Log in as client
-spotify = tekore.Spotify(token.access_token)
-
-
-def update_playlist(playlist_id: str,
-                    name: str | None = None,
-                    description: str | None = None,
-                    **kwargs,  # Ignore extraneous config like `comment`
-                    ) -> None:
-    """Update Spotify playlist details.
-
-    Args:
-        playlist_id (str): ID of the playlist to update.
-        name (str | None, optional): New name of the playlist. Defaults
-        to None, meaning keep the current name.
-        description (str | None, optional): New description of the
-        playlist. Defaults to None, meaning keep the current
-        description.
-    """
-    spotify.playlist_change_details(playlist_id=playlist_id,
-                                    name=name,  # type: ignore
-                                    description=description)  # type: ignore
